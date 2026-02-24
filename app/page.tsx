624138542a811717ac0d80cb1f355f8c551d1dd0
Sherif Altiby <sherifahmed852002@gmail.com>
2026-02-25 01:42:52 +0200

    finish the app

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./__components/Navbar";
 import Hero from "./__components/Hero";
+import Cocktails from "./__components/Cocktails";
+import About from "./__components/About";
+import Art from "./__components/Art";
+import Menu from "./__components/Menu";
+import Contact from "./__components/Contact";
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -11,7 +16,11 @@ export default function Home() {
         <main>
              <Navbar />
              <Hero />
-             <div className="h-screen bg-black" ></div>
+             <Cocktails />
+             <About />
+             <Art />
+             <Menu />
+             <Contact />
         </main>
     </div>
   );
